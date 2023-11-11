cb0fa2d0f27c258886b3d62e8011925de5e44f83
breno.breves <brenocsb@dcc.ufrj.br>
2023-11-11 13:07:26 -0300

    Algo deu errado nas configuraçãoes, recomeçando do 0

--- src/schemas/participant-schema.ts
+++
@@ -1,8 +0,0 @@
-import Joi from "joi";
-
-import { CreateParticipant } from "@/protocols";
-
-export const createParticipantSchema = Joi.object<CreateParticipant>({
-    name: Joi.string().required(),
-    balance: Joi.number().min(1000).required(),
-})
--- src/services/participants-service.ts
+++
@@ -1,8 +0,0 @@
-import { CreateParticipant } from "@/protocols";
-import { ParticipantRepository } from "@/repositories";
-
- async function createParticipant(participant: CreateParticipant){
-    return await ParticipantRepository.createParticipant(participant)
- }
-
- export const ParticipantService = { createParticipant }
